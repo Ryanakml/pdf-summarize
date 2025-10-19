495a4d5c0f5fe2e2669b2056b36413cb378f2cdb
RYAN AKMAL PASYA <brian.sbg12@gmail.com>
2025-10-19 13:27:48 +0700

    chore(ui): update Header component styling and logic

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
         <NavLink href="/#pricing">Pricing</NavLink>
         <SignedIn>
-          <NavLink href="/#dashboard">Your Summary</NavLink>
+          <NavLink href="/dashboard">Your Summary</NavLink>
         </SignedIn>
       </div>
 
